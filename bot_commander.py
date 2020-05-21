20d520c43a616b6c75f87391adf4cf48b58b6aee
Ernesto Mancebo <emancebo@neural.com.do>
2020-05-21 23:47:03 +0200

    adds bot_commander.py + some refactor

---
+++ bot_commander.py
@@ -0,0 +1,25 @@
+from main import (MODEL_NAME, build_seq2seq_model, init_inference, initial_setup,
+                  load_model_weights, load_vocabulary)
+
+CORPUS_NAME = 'twitter'
+
+# We're only interested in the metadata object
+metadata = initial_setup(CORPUS_NAME)[0]
+
+(word2idx, idx2word, unk_id, pad_id, start_id,
+ end_id, vocabulary_size) = load_vocabulary(metadata)
+
+model_ = build_seq2seq_model(vocabulary_size)
+load_model_weights(model_)
+
+seeds = ["come home", "hillary had nothing to do",
+         "only reason i can exercise is because i can breathe now", "i never heard of it until you mentioned it"]
+
+for seed in seeds:
+    print("Query >", seed)
+
+    inference = init_inference(model_, word2idx, idx2word, unk_id, start_id)
+    top_n = 3
+    for i in range(top_n):
+        sentence = inference(seed, top_n)
+        print(" >", ' '.join(sentence))
